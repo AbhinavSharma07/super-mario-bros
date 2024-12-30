0713ce87f255d060f056f3d1948b4c61d7c4e9f7
Abhinav Sharma <113385195+AbhinavSharma07@users.noreply.github.com>
2024-12-30 23:56:28 +0530

    Update mar.py

--- mar.py
+++ mar.py
@@ -1,21 +1,6 @@
 
 
 
-        else:
-            self.on_ground = False
-
-    def jump(self):
-        if self.on_ground:
-            self.vel_y = JUMP_SPEED
-
-# Platform class
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
-        super().__init__()
-        self.image = pygame.Surface((width, height))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
         self.rect.y = y
 
 # Groups
