412bc1f23cc8a797960ed41f54ea7aed0426bfa7
Abhinav Sharma <113385195+AbhinavSharma07@users.noreply.github.com>
2024-12-30 23:57:04 +0530

    Update mar.py

--- mar.py
+++ mar.py
@@ -1,12 +1,4 @@
-player, platforms, False):
-        player.vel_y = 0
 
-    # Draw
-    screen.fill(WHITE)
-    all_sprites.draw(screen)
-
-    # Refresh screen
-    pygame.display.flip()
 
 pygame.quit()
 sys.exit()
